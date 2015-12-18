b96ec1f28551cb13a6eafc0efd0765dfe8d0700f
frederick sandalo <frederick sandalo>
2015-12-18 21:41:29 +0800

    next item on the list

--- server.php
+++ server.php
@@ -1,12 +1,12 @@
 <?php
 	echo header('Content-Type: application/json');
 
-	if(isset($_POST['userID']))
+	if(isset($_POST['userID']) && $_POST['module'] == 'customerInformation' )
 	{
-		echo json_encode(array("customerID" => "value1",
-							   "personalCardNumber" => "value2",
-							   "lastOrderDate" => "value3",
-							   "registrationDate" => "value4",
+		echo json_encode(array("customerID" => "123456",
+							   "personalCardNumber" => "0123456",
+							   "lastOrderDate" => "2015/11/01",
+							   "registrationDate" => "2015/11/01",
 							   "customerName" => "XXXXXXXXXXXX",
 							   "phonetic" => "XXXXXXXXXXXXXX",
 							   "telephoneNumber1" => "999-999-9999",
@@ -14,14 +14,17 @@
 							   "streetAddress" => "京都府京都市上京区智恵光院通り芦山寺上る西入る西社町",
 							   "municipality" => "01:青葉区",
 							   "municipalityPostalCode" => "999-9999",
-							   "municipalityAddress" => "Sendai, Miyagi Prefecture, Aoba-ku",
+							   "municipalityAddress" => "Sendai, Miyagi Prefecture, Aoba-kuDDDD",
 							   "municipalityPhoneNumber" => "999-999-9999",
 							   "localGovernmentPersonnel" => "XXXXXXXXXXXX"
 							  )
 						);
 	}
 
-
-
-
+	
+	if(isset($_POST['userID']) && $_POST['module'] == 'ticketsUsedInformation')
+	{
+		echo json_encode(array("successFlag" => "success value from the tickets used information"));
+	}
+	
 ?>
